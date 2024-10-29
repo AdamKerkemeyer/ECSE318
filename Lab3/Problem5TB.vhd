4163645aa1ff4b268950dfa2ea84b42e2a9a83ea
Adam J Kerkemeyer <146299255+AdamKerkemeyer@users.noreply.github.com>
2024-10-29 13:52:13 -0400

    Updates

---
+++ Lab3/Problem5TB.vhd
@@ -0,0 +1,7 @@
+--Problem 5 TB
+--All libraries in ModelSim default compile to work so we can access anything compiled under that library.
+--No include managment, everything in the library you compile this to is automatically included.
+--Use "compile to library" in ModelSim to pick where to compile
+--I compiled all files to a folder called "Lab3" which is why I run
+--library Lab3;
+--use Problem5.all;
--- Lab3/Problem6.vhd
+++ Lab3/Problem6.vhd
@@ -2,8 +2,8 @@
 --Evaluate C = A*(B+2)
 --A,B,C stored in  memory locations 0, 1, and 2 respectively
 library IEEE; 
-use IEEE.STD_LOGIC_1164.ALL; --Need this package for definition of std_logic type
-use IEEE.STD_LOGIC_ARITH.ALL; --Need this package to do arithmatick for evaluate
+use IEEE.STD_LOGIC_1164.all; --Need this package for definition of std_logic type
+use IEEE.STD_LOGIC_ARITH.all; --Need this package to do arithmatick for evaluate
 --use IEEE.STD_LOGIC_UNSIGNED.ALL --Need this package to allow vectors to be evaluated as unsigned integers
 
 entity problem6 is 
