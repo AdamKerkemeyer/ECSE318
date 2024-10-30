10bca2dcd800f25b6b64ab84ee8b5ec7d2d9021b
Adam J Kerkemeyer <146299255+AdamKerkemeyer@users.noreply.github.com>
2024-10-30 18:17:38 -0400

    TB

--- Lab3/Problem1TB.vhd
+++ Lab3/Problem1TB.vhd
@@ -1,46 +1,67 @@
-library ieee;
-use ieee.STD_LOGIC_1164.all;
-use ieee.numeric_std.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity Problem1TB is 
-end entity
+entity Problem1TB is
+end Problem1TB;
 
-entity MuxTB is 
-end entity
+architecture sim of Problem1TB is
+    -- Component declaration for the unit under test (UUT)
+    component problem1
+        Port ( x : in  STD_LOGIC_VECTOR (7 downto 0);
+               y : in  STD_LOGIC_VECTOR (7 downto 0);
+               cin : in  STD_LOGIC;
+               cout : out  STD_LOGIC;
+               correctSum : out  STD_LOGIC_VECTOR (7 downto 0));
+    end component;
 
-architecture sim of Mux is
-    constant DataWidth : integer := 8; --Now our multiplexer is generic
+    -- Signals to connect to the UUT
+    signal x : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
+    signal y : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
+    signal cin : STD_LOGIC := '0';
+    signal cout : STD_LOGIC;
+    signal correctSum : STD_LOGIC_VECTOR (7 downto 0);
 
-    signal Sig1 : unsigned(DataWidth-1 downto 0) := x"AA";
-    signal Sig2 : unsigned(DataWidth-1 downto 0) := x"BB";
-    signal Sig3 : unsigned(DataWidth-1 downto 0) := x"CC";
-    signal Sig4 : unsigned(DataWidth-1 downto 0) := x"DD";
-    signal Sel : unsigned(1 downto 0) := (others => '0');
-    signal Output : unsigned(DataWidth-1 downto 0);
 begin
-    --Instance of Multiplexer module
-    i_Mux1 : entity work.Mux(rtl) 
-    generic map(DataWidth => DataWidth)
-    port map(
-        Sel => Sel, 
-        Sig1 => Sig1, 
-        Sig2 => Sig2,
-        Sig3 => Sig3,
-        Sig4 => Sig4,
-        Output => Output);
-    --Testbench Process
-    process is
-    begin 
+
+    -- Instantiate the Unit Under Test (UUT)
+    uut: problem1 Port map (
+        x => x,
+        y => y,
+        cin => cin,
+        cout => cout,
+        correctSum => correctSum
+    );
+
+    -- Stimulus process
+    stim_proc: process
+    begin
+        -- Test vector 1
+        x <= "00000001"; y <= "00000001"; cin <= '0';
         wait for 10 ns;
-        Sel <= Sel +1;
+        
+        -- Test vector 2
+        x <= "00001111"; y <= "00001111"; cin <= '1';
         wait for 10 ns;
-        Sel <= Sel +1;
+        
+        -- Test vector 3
+        x <= "10101010"; y <= "01010101"; cin <= '0';
         wait for 10 ns;
-        Sel <= Sel +1;
+        
+        -- Test vector 4
+        x <= "11111111"; y <= "00000001"; cin <= '1';
         wait for 10 ns;
-        Sel <= Sel +1;
+        
+        -- Test vector 5
+        x <= "11110000"; y <= "00001111"; cin <= '0';
         wait for 10 ns;
-        Sel <= "UU";
-        wait
+        
+        -- Add more test vectors as needed
+        -- ...
+
+        -- End simulation
+        wait;
     end process;
-end architecture;
+
+end Behavioral;
