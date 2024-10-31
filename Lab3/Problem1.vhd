5dfb78da3bb3c980a94b487369efebe55d3c0d7c
Adam J Kerkemeyer <146299255+AdamKerkemeyer@users.noreply.github.com>
2024-10-31 11:44:28 -0400

    Problem1 Compiles Now

--- Lab3/Problem1.vhd
+++ Lab3/Problem1.vhd
@@ -24,11 +24,11 @@ architecture rtl of Problem1 is
             variable result : std_logic_vector(in0_data'length downto 0); --1 longer than out_data
     begin
         if sel = '1' then
-            out_data <= in1_data;
-            out_carry <= in1_carry;
+            out_data := in1_data; --if you use <= it is a compilation error
+            out_carry := in1_carry;
         else
-            out_data <= in0_data;
-            out_carry <= in0_data;
+            out_data := in0_data;
+            out_carry := in0_carry;
         end if;
         result := out_carry & out_data;
         return result;
@@ -37,90 +37,88 @@ architecture rtl of Problem1 is
     function fullAdder(a, b, cin : in std_logic) return std_logic_vector is
         variable sum, cout : std_logic; --going to combine them together for the output
     begin --Will run in 0 time because it is a function but this is ok because 
-        sum <= a xor b xor cin;
-        cout <= (a and b) or (a and cin) or (b and cin);
+        sum := a xor b xor cin;
+        cout := (a and b) or (a and cin) or (b and cin);
         return (cout & sum);
-    end function
+    end function;
     --Signal declaration
     signal carry1, carry0 : std_logic_vector(8 downto 0); --first round of carrys
     signal sum1, sum0 : std_logic_vector(7 downto 0); --first round of sums
     signal muxCarry1, muxCarry0 : std_logic_vector(8 downto 0); --first round of mux selection
     signal muxSum1, muxSum0 : std_logic_vector(7 downto 0); --first round of mux selection
-    signal muxCarrySecond1, muxCarrySecond0 : std_logic_vector(8 downto 0);--second round of mux selection
+    signal muxCarrySecond1, muxCarrySecond0 : std_logic;--second round of mux selection
     signal muxSumSecond1, muxSumSecond0 : std_logic_vector(7 downto 0);--second round of mux selection
     signal correctCarry : std_logic_vector(8 downto 0);--store the known correctCarry here, correlates with correctSum
 
+    signal fullAdderResult0, fullAdderResult1 : std_logic_vector(1 downto 0); --For holding intermediate full adder
+    signal mux_result : std_logic_vector(1 downto 0);  --For holding 2 bit n_bit_mux return value
+    signal mux_result2 : std_logic_vector(2 downto 0); --For holding 3 bit n_bit_mux return values
+    signal mux_result3 : std_logic_vector(4 downto 0); --For holding 5 bit n_bit_mux return values
+    --I think another way to do this would be to run the function call 2x and just assign 1 bit at a time to the output
+    --Technically this might be less computationally intensive at the cost of being less space efficient
 begin
     --it makes the most sense to me to write the full adder and n_bit_mux as functions to me
     --because I can avoid using processes then which seem to be larger while accomplishing the same thing
     --not sure I understand the difference well enough however
 
     --x0y0 special case
-    (corectSum(0), correctCarry(1)) <= full_adder(y(0), x(0), cin); --left item is assigned LSB function output
+    (correctSum(0), correctCarry(1)) <= fullAdder(y(0), x(0), cin); --left item is assigned LSB function output
     --if above does not work I will use two lines:
     --correctSum(0) <= full_adder(y(0), x(0), cin)(0);
     --correctCarry(1) <= full_adder(y(0), x(0), cin)(1);
-    generate first row of full adders
+    --generate first row of fullAdders
     generate_adders : for i in 1 to 7 generate
-        (sum0(i), carry0(i+1)) <= full_adder(y(i), x(i), '0');
-        (sum1(i), carry1(i+1)) <= full_adder(y(i), x(i), '1');
+        --(sum0(i), carry0(i+1)) <= fullAdder(y(i), x(i), '0'); --Why does this not work?
+        --(sum1(i), carry1(i+1)) <= fullAdder(y(i), x(i), '1');
+	fullAdderResult0 <= fullAdder(y(i), x(i), '0');
+        fullAdderResult1 <= fullAdder(y(i), x(i), '1');
+        sum0(i) <= fullAdderResult0(0);
+        carry0(i+1) <= fullAdderResult0(1);
+        sum1(i) <= fullAdderResult1(0);
+        carry1(i+1) <= fullAdderResult1(1);
     end generate;
     --x1y1 special case --in0 data should always come before in1 data
-    (correctSum(1), correctCarry(2)) <= n_bit_mux(sum0(1), carry0(2), sum1(1), carry1(2), correctCarry(1));
+    --n_bit_mux expecta std_logic_vector as input, and if I just say sum0(1) this will input an std_logic which it cannot use
+    --because VHDL is stringly typed, but if I say 1 downto 1 it will maintain its status as a std_logic_vector and VHDL happy
+    -- Assign the output of n_bit_mux to the temporary signal to prevent slice compilation issues
+    mux_result <= n_bit_mux(sum0(1 downto 1), carry0(2), sum1(1 downto 1), carry1(2), correctCarry(1));
+    -- Assign slices of mux_result to `correctSum` and `correctCarry`
+    correctSum(1) <= mux_result(0);
+    correctCarry(2) <= mux_result(1);
 
     --First round of intermediate muxes:
     generate_muxes: for j in 3 to 7 generate
-        if j mod 2 = 1 then --because I don't know how to increment the for loop by 2
-            (muxSum0(j), muxCarry0(j+1)) <= n_bit_mux(sum0(j), carry0(j+1), sum1(j), carry1(j+1), carry0(j));
-            (muxSum1(j), muxCarry1(j+1)) <= n_bit_mux(sum0(j), carry0(j+1), sum1(j), carry1(j+1), carry1(j));
-        end if;
+	--Not allowed to place if statement in the generate block because it is in a concurrent statement.
+	--Can use an if generate without the for loop to get around this, so only one generate per outer generate loop
+        odd_index: if j mod 2 = 1 generate --because I don't know how to increment the for loop by 2
+        mux_result <= n_bit_mux(sum0(j downto j), carry0(j+1), sum1(j downto j), carry1(j+1), carry0(j));
+        -- Distribute mux_result slices to muxSum0 and muxCarry0
+        muxSum0(j) <= mux_result(0);
+        muxCarry0(j+1) <= mux_result(1);
+        -- Repeat for muxSum1 and muxCarry1
+        mux_result <= n_bit_mux(sum0(j downto j), carry0(j+1), sum1(j downto j), carry1(j+1), carry1(j));
+        muxSum1(j) <= mux_result2(0);
+        muxCarry1(j+1) <= mux_result2(1);
+        end generate;
     end generate;
 
-    --Sum[3:2] multiplexer special case
-    (correctSum(3 downto 2), correctCarry(4)) <= n_bit_mux(muxSum0(3) & sum0(2), carry0(4), (muxSum1(3) & sum1(2)), carry1(4), correctCarry(2));
+    --Sum[3:2] multiplexer special case, maintain all std_logic_vectors as vectors by using downto
+     mux_result2 <= n_bit_mux(muxSum0(3 downto 3) & sum0(2 downto 2), carry0(4), (muxSum1(3 downto 3) & sum1(2 downto 2)), carry1(4), correctCarry(2));
+    correctSum(3 downto 2) <= mux_result2(1 downto 0);
+    correctCarry(4) <= mux_result2(2);
     --correctSum(3 downto 2) <= n_bit_mux(muxSum0(3 downto 3) & sum0(2 downto 2), carry0(4), muxSum1(3 downto 3) & sum1(2 downto 2), carry1(4), correctCarry(2))(1 downto 0);
     --correctCarry(4) <= n_bit_mux(muxSum0(3 downto 3) & sum0(2 downto 2), carry0(4), muxSum1(3 downto 3) & sum1(2 downto 2), carry1(4), correctCarry(2))(2);
     --last 3 muxes:
-    (muxSumSecond0(7 downto 6), muxCarrySecond0) <= n_bit_mux(muxSum0(7 downto 6), muxCarry1(8), muxSum1(7 downto 6), muxCarry0(8), muxCarry0(6));
-    (muxSumSecond1(7 downto 6), muxCarrySecond1) <= n_bit_mux(muxSum0(7 downto 6), muxCarry1(8), muxSum1(7 downto 6), muxCarry0(8), muxCarry1(6));
+    mux_result2 <= n_bit_mux(muxSum0(7 downto 6), muxCarry1(8), muxSum1(7 downto 6), muxCarry0(8), muxCarry0(6));
+    muxSumSecond0(7 downto 6) <= mux_result2(1 downto 0);
+    muxCarrySecond0 <= mux_result2(2);
 
+    mux_result2 <= n_bit_mux(muxSum0(7 downto 6), muxCarry1(8), muxSum1(7 downto 6), muxCarry0(8), muxCarry1(6));
+    muxSumSecond1(7 downto 6) <= mux_result2(1 downto 0);
+    muxCarrySecond1 <= mux_result2(2);
     -- Final mux
-    (correctSum(7 downto 4), cout) <= n_bit_mux(muxSumSecond0(7 downto 6) & muxSum0(5 downto 5) & sum0(4 downto 4), 
-                    muxCarrySecond1, muxSumSecond1(7 downto 6) & muxSum1(5 downto 5) & sum1(4 downto 4), muxCarrySecond0, correctCarry(4));
-
+    mux_result3 <= n_bit_mux((muxSumSecond0(7 downto 6) & muxSum0(5 downto 5) & sum0(4 downto 4)), 
+                    muxCarrySecond1, (muxSumSecond1(7 downto 6) & muxSum1(5 downto 5) & sum1(4 downto 4)), muxCarrySecond0, correctCarry(4));
+    correctSum(7 downto 4) <= mux_result3(4 downto 1);
+    cout <= mux_result3(0);
 end architecture;
-
---Generic Multiplexer
-entity Mux is
-generic(DataWidth : integer); --Minus 1 as it is inclusive of MSB and LSB
-port(
-    --Inputs
-    signal Sig1 : in unsigned(DataWidth-1 downto 0);
-    signal Sig2 : in unsigned(DataWidth-1 downto 0);
-    signal Sig3 : in unsigned(DataWidth-1 downto 0);
-    signal Sig4 : in unsigned(DataWidth-1 downto 0);
-
-    signal Sel : in unsigned(1 downto 0);
-    --Outputs
-    signal Output : out unsigned(DataWidth-1 downto 0));
-end entity
-
-architecture rtl of Mux is
-begin --register transfer level
-    --MUS using case statement
-    process(Sel, Sig1, Sig2, Sig3, Sig4) is
-    begin
-        case Sel is
-            when "00" => 
-                Output <= Sig1;
-            when "01" => 
-                Output <= Sig2;
-            when "10" => 
-                Output <= Sig3;
-            when "11" => 
-                Output <= Sig4;
-            when others => -- 'U' 'X' 
-                Output1 <= (others => 'X');
-        end case;
-    end process;
-end architecture rtl;
--- Lab3/Problem1TB.vhd
+++ Lab3/Problem1TB.vhd
@@ -1,14 +1,15 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_TEXTIO.ALL;
+use STD.TEXTIO.ALL;
 
 entity Problem1TB is
 end Problem1TB;
 
 architecture sim of Problem1TB is
     -- Component declaration for the unit under test (UUT)
-    component problem1
+    component Problem1
         Port ( x : in  STD_LOGIC_VECTOR (7 downto 0);
                y : in  STD_LOGIC_VECTOR (7 downto 0);
                cin : in  STD_LOGIC;
@@ -22,11 +23,12 @@ architecture sim of Problem1TB is
     signal cin : STD_LOGIC := '0';
     signal cout : STD_LOGIC;
     signal correctSum : STD_LOGIC_VECTOR (7 downto 0);
+    signal expectedSum : STD_LOGIC_VECTOR (7 downto 0);
 
 begin
 
     -- Instantiate the Unit Under Test (UUT)
-    uut: problem1 Port map (
+    uut: Problem1 Port map (
         x => x,
         y => y,
         cin => cin,
@@ -36,18 +38,34 @@ begin
 
     -- Stimulus process
     stim_proc: process
+        variable i, j : integer;
+        variable l : line;
+        variable expectedSum_str, correctSum_str : string(1 to 8);
     begin
-    -- Loop through all possible combinations of x and y
+        -- Loop through all possible combinations of x and y
         for i in 0 to 255 loop
             for j in 0 to 255 loop
                 x <= std_logic_vector(to_unsigned(i, 8));
                 y <= std_logic_vector(to_unsigned(j, 8));
                 cin <= '0';  -- You can also loop through cin if needed
                 wait for 10 ns;
+                -- Calculate expected sum
+                expectedSum <= std_logic_vector(to_unsigned(i + j, 8));
+                -- Convert std_logic_vector to string
+                write(l, expectedSum);
+                expectedSum_str := l.all;
+                write(l, correctSum);
+                correctSum_str := l.all;
+                -- Check if correctSum matches expectedSum
+                if correctSum /= expectedSum then
+                    report "Mismatch: x=" & integer'image(i) &
+                           ", y=" & integer'image(j) &
+                           ", expectedSum=" & expectedSum_str &
+                           ", correctSum=" & correctSum_str severity error;
+                end if;
             end loop;
         end loop;
         -- End simulation
         wait;
     end process;
-
-end Behavioral;
+end sim;
