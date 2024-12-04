32809ea1447ec4a158a80d5e75bf5d826d00a308
Evan-Grover <ehgrover123@gmail.com>
2024-12-03 22:01:55 -0500

    Wtring out some of what Ive got to do

--- Lab5/Simlulator.cpp
+++ Lab5/Simlulator.cpp
@@ -1,4 +1,22 @@
-#include ECSE318Sim
+#include "Simlulator.hpp"
 
-using namespace ECSE318Sim;
+Simulator::Simulator(const std::vector<Gate*> Gates, const std::string& testfile, const std::string& gatefile){
+    //Read test file into stimulus datastructure
+    //Create a bunch of gates and put them into the gate data sturcture
+    //Check if loading in worked, number of input chars should equal num of inputs.
+}
+Simulator::~Simulator(){
+    //Learn to use smart pointers not a destructor
+}
 
+void Simulator::runSim(){
+    //ask for output file name
+    //Gates should be intitialized to X as state
+    //Calls a for loop to iterate through each line of the stimulus file
+    //take the output of runCycle and stick it in the output file
+}
+
+std::vector<logic>[] Simulator::runCycle(std::vector<char>*){
+    //Assign new input stimulus to input gates
+    //Go level by level evaluating each gate
+}
--- Lab5/Simlulator.hpp
+++ Lab5/Simlulator.hpp
@@ -5,8 +5,7 @@
 #include <vector>
 #include <string>
 
-namespace ECSE381Sim
-{
+
 
 enum class logic{//Three valued logic has... three values
     zero = 0,
@@ -39,17 +38,33 @@ const logic xorTable[3][3] = {
 
 const logic notTable[3] = {logic::one, logic::zero, logic::X};
 
-logic inputScan();
-logic tableLookup();
+logic inputScanEvaluate();
+logic tableEvaluate();
+
+logic checkTable(const GateType type, const logic in1, const logic in2){//evaulates two gates based on table lookup
+    switch(type){
+        case GateType::AND:
+            return andTable[static_cast<int>(in1)][static_cast<int>(in2)];
+        case GateType::OR:
+            return orTable[static_cast<int>(in1)][static_cast<int>(in2)];
+        case GateType::NOT:
+            return notTable[static_cast<int>(in1)];
+        case GateType::NOR:
+            return notTable[static_cast<int>(orTable[static_cast<int>(in1)][static_cast<int>(in2)])];
+        case GateType::NAND:
+            return notTable[static_cast<int>(andTable[static_cast<int>(in1)][static_cast<int>(in2)])];
+    }
+}
 
 class Simulator{
     public:
         Simulator(const std::vector<Gate*> Gates, const std::string& testfile, const std::string& gatefile);
+        ~Simulator();
         void runSim();//Top level function. Call to begin running the simulation
-        std::vector<logic> runCycle();//Runs a single clock cycle. Returns list of next dff values
+        std::vector<logic> runCycle(std::vector<char>*);//Runs a single clock cycle. Returns list of next dff values
     private:
-        std::vector<Gate*> Gates;
-        std::vector<std::vector<logic>> stimulus;
+        std::vector<std::vector<Gate*>>* Gates = nullptr; //Each top level is a list of the gates of a given level
+        std::vector<std::vector<char>>* stimulus = nullptr; //Each entry in the top vector is a line. Then each line is just an array of chars
 };
-}
+
 #endif
