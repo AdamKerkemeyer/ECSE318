9970d251cd46fe667e41ea070b475d9d163b2b23
Adam J Kerkemeyer <146299255+AdamKerkemeyer@users.noreply.github.com>
2024-12-04 12:55:22 -0500

    Refresh

---
+++ Lab5/Simlulator.cpp
@@ -0,0 +1,6 @@
+/*
+How to prove a line is redundant (we can remove a gate)
+If input is redundant it can be replaced with a 1 or 0 (if it is inverting, replace the gate with an inverter)
+D front: error on input -> x on output
+
+*/
