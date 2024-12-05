18a00543fd031547eb96956b0901084984d01fc1
Evan-Grover <ehgrover123@gmail.com>
2024-12-04 22:40:28 -0500

    adding string functions!

--- Lab5/Simlulator.cpp
+++ Lab5/Simlulator.cpp
@@ -1,12 +1,47 @@
 #include "Simlulator.hpp"
+#include <vector>
+#include <fstream>
+#include <string>
+#include <iostream>
 
-Simulator::Simulator(const std::vector<Gate*> Gates, const std::string& testfile, const std::string& gatefile){
+Simulator::Simulator(const std::string& testfile, const std::string& gatefile){
+    std::ifstream file(gatefile);       //Lets us read lines from a file into a string
+    if (!file.is_open()) {
+        std::cout << "Error opening file: " << gatefile << std::endl;
+        return;                         //Use cerr to output an error message if something goes wrong (not buffered like cout is)
+    }
+    std::string line = "";
+    std::string type = "";
+    std::string name = "";
+    while (std::getline(file, line)) {
+
+    }
+
     //Read test file into stimulus datastructure
     //Create a bunch of gates and put them into the gate data sturcture
     //Check if loading in worked, number of input chars should equal num of inputs.
 }
-Simulator::~Simulator(){
-    //Learn to use smart pointers not a destructor
+
+std::vector<std::string> Simulator::gateData(const std::string& target, std::string& line){
+    size_t startPos = line.find(target + "{");
+    size_t endPos = line.find("}");
+    size_t inputLength = target.length();
+    std::string valString = line.substr(startPos + inputLength + 1, endPos - (startPos + inputLength + 1));
+    if (valString.find(",") == std::string::npos){
+        return std::vector(1, valString);
+    }
+    else{
+        std::vector<std::string> result(1);
+        size_t newpos = 0;
+        size_t oldpos = 0;
+        while (newpos != std::string::npos){
+            newpos = valString.find(",",oldpos);
+            result.push_back(valString.substr(oldpos, newpos-oldpos));
+            oldpos = newpos;
+        }
+        return result;
+    }
+
 }
 
 void Simulator::runSim(){
@@ -16,7 +51,7 @@ void Simulator::runSim(){
     //take the output of runCycle and stick it in the output file
 }
 
-std::vector<logic>[] Simulator::runCycle(std::vector<char>*){
+std::vector<logic> Simulator::runCycle(std::vector<char>*){
     //Assign new input stimulus to input gates
     //Go level by level evaluating each gate
 }
--- Lab5/Simlulator.hpp
+++ Lab5/Simlulator.hpp
@@ -4,6 +4,7 @@
 #include "Gate.hpp"
 #include <vector>
 #include <string>
+#include <memory>
 
 
 
@@ -41,7 +42,7 @@ const logic notTable[3] = {logic::one, logic::zero, logic::X};
 logic inputScanEvaluate();
 logic tableEvaluate();
 
-logic checkTable(const GateType type, const logic in1, const logic in2){//evaulates two gates based on table lookup
+logic checkTable(const GateType& type, const logic& in1, const logic& in2){//evaulates two gates based on table lookup
     switch(type){
         case GateType::AND:
             return andTable[static_cast<int>(in1)][static_cast<int>(in2)];
@@ -58,13 +59,14 @@ logic checkTable(const GateType type, const logic in1, const logic in2){//evaula
 
 class Simulator{
     public:
-        Simulator(const std::vector<Gate*> Gates, const std::string& testfile, const std::string& gatefile);
-        ~Simulator();
+        Simulator(const std::string& testfile, const std::string& gatefile);
         void runSim();//Top level function. Call to begin running the simulation
-        std::vector<logic> runCycle(std::vector<char>*);//Runs a single clock cycle. Returns list of next dff values
+        std::vector<logic> runCycle(std::vector<char>*);//Runs a single clock cycle. Returns list of output values, dff vals are stored in the dffs themselves
     private:
-        std::vector<std::vector<Gate*>>* Gates = nullptr; //Each top level is a list of the gates of a given level
-        std::vector<std::vector<char>>* stimulus = nullptr; //Each entry in the top vector is a line. Then each line is just an array of chars
+        std::unique_ptr<std::vector<std::vector<std::unique_ptr<Gate>>>> Gates = nullptr; //Each top level is a list of the gates of a given level
+        std::unique_ptr<std::vector<std::vector<char>>> stimulus = nullptr; //Each entry in the top vector is a line. Then each line is just an array of chars
+
+        std::vector<std::string> gateData(const std::string& target, std::string& line);//returns a string of the value, modifies the line to take out the value
 };
 
 #endif
